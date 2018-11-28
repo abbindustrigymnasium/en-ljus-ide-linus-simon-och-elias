8eca0f1049174a87ae45132d780eb5147d8725ba
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2018-11-28 13:19:00 +0100

    Update Func.timer.ino

--- Arduino/avst_ndsgivareHand/Func.timer/Func.timer.ino
+++ Arduino/avst_ndsgivareHand/Func.timer/Func.timer.ino
@@ -1,4 +1,4 @@
-
+int i = 0;
 int Mintimer = 23;
 bool powervalue = false ;
 void setup() {
@@ -6,20 +6,29 @@ Serial.begin(9600);
   // put your setup code here, to run once:
  // Serial.println(Mintimer);
 };
-for (int  i=0; i <= Mintimer; i++){
-  powervalue=true; 
-  delay(6000);
-  Serial.println(Mintimer);
-  Serial.println(i);
-  Serial.println(powervalue);
-};
-powervalue=false;
-Serial.println(powervalue);
 
 void loop() {
 
   // put your main code here, to run repeatedly:
 
-
+ while ( i <= Mintimer ){
+  delay(600);
+  powervalue=true;
+  Serial.println(Mintimer);
+  Serial.println(i);
+  Serial.println(powervalue);
+  i++;
+ }
+ while (i > Mintimer) {
+powervalue=false;
+delay(200);
+Serial.println(powervalue);
+Serial.print("abcderfh");
+break;
 }
+
+
+ }
+
+
  
