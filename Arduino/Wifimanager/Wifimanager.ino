bf48cc72179efd1954df5eeb48ef257da65b5be1
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2018-12-12 13:20:09 +0100

    Wifimanager concluding :)

--- Arduino/Wifimanager/Wifimanager.ino
+++ Arduino/Wifimanager/Wifimanager.ino
@@ -7,6 +7,36 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
+void setup() {
+    pinMode(13,OUTPUT);
+    
+    Serial.begin(115200);
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+    //reset saved settings
+    //wifiManager.resetSettings();
+    
+    //set custom ip for portal
+    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //fetches ssid and pass from eeprom and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    wifiManager.autoConnect("Elias_Wajfai");
+    //or use this for auto generated name ESP + ChipID
+    //wifiManager.autoConnect();
+
+    
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+}
+
+
+
+
 String Lampname = "1";
 bool Powervalue = false;
 bool SensorValue = false;
@@ -14,37 +44,16 @@ int Coldvalue = 0;
 int Hotvalue = 0; 
 bool LampExist = false;
 bool GottenValues = false;
-      
-void ConnecttoDB(String input){
-  
-      const int httpPort=3001;
-      const char*host="192.168.0.100";
 
-      Serial.print("connecting to ");
-      Serial.println(host);
 
-      WiFiClient client;
-      if(!client.connect(host,httpPort)){
-      Serial.println("connection failed");
-      return;
-      } else {
-      digitalWrite(13,HIGH); //123
-      }
-  
-  if(input =="GET")
-  client.print(GetfromDB(host));
-  else
-  client.print(SendtoDB(host));
-  unsigned long timeout = millis();
-    while (client.available() == 0) {
-      if (millis() - timeout > 10000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-    return;
-    }
-    }
-  }
-    
+String GetfromDB(String host){
+String url="/ddosmonster/"+Lampname;
+  String Output="GET"+url+"HTTP/1.1\r\n"+
+     "Host:"+host+"\r\n"+
+     "\r\nConnection:close\r\n\r\n";
+  return Output;
+}
+
 String SendtoDB(String host){
   String type="POST";
   if(GottenValues==true) {
@@ -74,14 +83,59 @@ String SendtoDB(String host){
   else
   return"";
 }
+void ConnecttoDB(String input){
+      const int httpPort=3001;
+      const char*host="192.168.0.100";
 
-String GetfromDB(String host){
-  String url="/ddosmonster/"+Lampname;
-  String Output="GET"+url+"HTTP/1.1\r\n"+
-  "Host:"+host+"\r\n"+
-  "\r\nConnection:close\r\n\r\n";
-  return Output;
+      Serial.print("connecting to ");
+      Serial.println(host);
+
+      WiFiClient client;
+      if(!client.connect(host,httpPort)){
+      Serial.println("connection failed");
+      return;
+      } else {
+      digitalWrite(13,HIGH); //123
+      }
+  
+  if(input =="GET")
+  client.print(GetfromDB(host));
+  else
+  client.print(SendtoDB(host));
+  unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 10000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+    return;
+    }
+    }
+   
+ String json = ""; //De delarna vi vill ha ut av meddelandet sparar vi i stringen json
+boolean httpBody = false; //bool för att säa att vi har kommit ner till bodydelen
+// tittar om vi har anslutit till clienten
+while (client.available()) {
+  String line = client.readStringUntil('\r'); //Läser varje rad tills det är slut på rader
+  if (!httpBody && line.charAt(1) == '{') { //Om vi hittar { så vet vi att vi har nått bodyn
+    httpBody = true; //boolen blir true för att vi ska veta för nästa rad att vi redan är i bodyn
+  }
+  if (httpBody) { //Om bodyn är sann lägg till raden i json variabeln
+    json += line;
+  }
 }
+//Skriver ut bodyns data
+    Serial.println("Got data:");
+    Serial.println(json);
+  if(input =="GET") //Om det är Get så kör vi metoden UpdateValues
+    UpdateValues(json);
+
+  Serial.println();
+  Serial.println("closing connection");
+}
+  }
+    
+
+
 
 void UpdateValues(String json){
   StaticJsonBuffer<400>jsonBuffer;
@@ -104,36 +158,10 @@ void UpdateValues(String json){
 }
 
 void UpdatingLamp(){
-  analogWrite(13, Strvalue);
-  Serial.println(Strvalue);
+  analogWrite(13, Coldvalue);
+  Serial.println(Coldvalue);
 }
 
-void setup() {
-    pinMode(13,OUTPUT);
-    
-    Serial.begin(115200);
-
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
-    //reset saved settings
-    //wifiManager.resetSettings();
-    
-    //set custom ip for portal
-    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
-    //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("Elias_Wajfai");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
-
-    
-    //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
-}
 
 void loop() {
     ConnecttoDB("GET");
