cf7854996b54c258990c03cdbdde250202a90ef4
ABBelinyq <43879756+ABBelinyq@users.noreply.github.com>
2018-12-17 18:14:34 +0100

    it fucking works, poggers

--- Arduino/Wifimanager/Wifimanager.ino
+++ Arduino/Wifimanager/Wifimanager.ino
@@ -1,17 +1,15 @@
-#include <ArduinoJson.h>
-
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino //needed for library
-
+#include <ESP8266WiFi.h>     
 //Både ArduinoJson och Wifimanager måste installeras som bibliotek, de finns med i bibliotekskatalogen, tänk att ArduinoJSon versionen som ska väljas är 5.13 och inte senaste.     
 #include <ArduinoJson.h> // V 5.13 inte 6! https://arduinojson.org/?utm_source=meta&utm_medium=library.properties
 //needed for library
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-
+#define D7 13 // SPI Bus MOSI
 
 void setup() {
-     
+      pinMode(13, OUTPUT); //Declare GPIO13 as output
+ 
     // put your setup code here, to run once:
     Serial.begin(115200);
   //Från Wifimanagers hemsida.
@@ -25,11 +23,11 @@ void setup() {
     //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
     //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified LampLampName
+    //if it does not connect it starts an access point with the specified name
     //here  "AutoConnectAP"
     //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("Eliasdab");
-    //or use this for auto generated Name ESP + ChipID
+    wifiManager.autoConnect("Connecttor");
+    //or use this for auto generated name ESP + ChipID
     //wifiManager.autoConnect();
 
     //if you get here you have connected to the WiFi
@@ -37,39 +35,38 @@ void setup() {
 
 }
 
-String LampName = "A";
-bool Powervalue = false;
-bool SensorValue = false;
-int Timer1Min = 0; 
-int Coldvalue = 0;
-int Hotvalue = 0; 
-bool LampExist = false;
-bool GottenValues = false;
-
+ String Lampname="A"; //Lampans namn
+ int Coldvalue= 0; //Hårdheten
+ int Hotvalue= 0; //Styrkan
+ bool Powervalue = 0; 
+ bool Sensorsetting = 0; 
+ int Timervalue = 0;  
+ bool LampExist=false; //Finns lampan redan eller är den ny?
+ bool GottenValues = false; //Har vi hämtat några värden redan från databasen?
 
 String GetfromDB(String host){
-String url = "/products/"+LampName;
-  String Output= "GET"+ url +"HTTP/1.1\r\n"+
-     "Host:"+ host +"\r\n"+
-     "\r\nConnection:close\r\n\r\n";
-  return Output;
-  delay(1000);
+String url= "/products/"+Lampname; //Urlen jag använder för att posta mina värden
+  // Detta skickar värdena till servern.
+   String Output ="GET "+ url + " HTTP/1.1\r\n" + //Säger att det är typen post, kan vara patch, get,delete beroende på vad man vill göra., samt urlen vi ska till.
+                 "Host: " + host+ "\r\n" + //Berättar vilken host det är vi ansluter till
+                 "\r\nConnection: close\r\n\r\n"; //skickar vår buffer som  body
+ return Output;
 }
 
 String SendtoDB(String host){
-  String type ="POST";
+  String type ="POST ";
   if(GottenValues==true)
   {
-  String url= "/products/"; //Urlen jag använder för att posta mina värden
+  String url= "/light/"; //Urlen jag använder för att posta mina värden
    
   StaticJsonBuffer<300> jsonBuffer; //Skapar en buffer, det vill säga så mycket minne som vårt blivande jsonobjekt får använda.
   JsonObject& root = jsonBuffer.createObject(); //Skapar ett jsonobjekt som vi kallar root
-   root["LampName"]=LampName;
-      root["Power"]=Powervalue;
-      //root["Timer1min"]=Timervalue;//
-      root["Hot"]=Hotvalue;
-      root["Cold"]=Coldvalue;
-      root["SensorSetting"]=SensorValue;
+  root["Name"] = Lampname; //Skapar parameterna name och ger den värdet Vykort
+  root["Hot"] = Hotvalue;
+  root["Cold"] = Coldvalue;
+  root["Timer1min"] = Timervalue;
+  root["SensorSetting"]= Sensorsetting;
+  root ["Power"] = Powervalue; // Samma som ovan
   String buffer;  //Skapar en string som vi kallar buffer
   root.printTo(buffer); //Lägger över och konverterar vårt jsonobjekt till en string och sparar det i buffer variabeln.
   if(LampExist==true)
@@ -77,28 +74,25 @@ String SendtoDB(String host){
   type ="PATCH ";
       Serial.println("Uppdaterar värdet!");
   }
- 
 //här någonstans ska jag anvädna POST eller PATCH beroende på om värdet finns!!!!
   // Detta skickar värdena till servern.
    String Output =type+url + " HTTP/1.1\r\n" + //Säger att det är typen post, kan vara patch, get,delete beroende på vad man vill göra., samt urlen vi ska till.
                  "Host: " + host+ "\r\n" + //Berättar vilken host det är vi ansluter till
-                 "Content-Type: application/json\r\n" + //Säger att det är Json format vi skickar (dock konverterat till en string för att kunna skickas).
+                 "Content-Type: application/json\r\n" + //Säger att det är Json format vi skickar (dock konverterat till en string för att kunna skickas.
                  "Content-Length: " + buffer.length() + "\r\n" + //Berättar hur stort packet vi ska skicka.
                  "\r\n" + // Detta är en extra radbrytning för att berätta att det är här bodyn startar.
                  buffer + "\n"; //skickar vår buffer som  body
-
+ 
  return Output;
   }
   else
   return "";
 }
 
-
-
 void ConnecttoDB(String input){
 
    const int httpPort = 3000; //porten vi ska till
-  const char* host = "http://iot.abbindustrigymnasium.se";//Adressen vi ska ansluta till. 7Laddaremygglustbil "http://iot.abbindustrigymnasium.se"
+  const char* host = "iot.abbindustrigymnasium.se";//Adressen vi ska ansluta till. 7Laddaremygglustbil "http://iot.abbindustrigymnasium.se" 193.41.215.179b
     
      Serial.print("connecting to ");
  Serial.println(host); //Skriver ut i terminalen för att veta vart vi ska skicka värdena.
@@ -111,11 +105,11 @@ void ConnecttoDB(String input){
   }
   else  //Om vi kan ansluta så ska lampa lysa
   {
-Serial.println("connected");
-}
-if(input =="GET"){
+    Serial.println("connected");//digitalWrite(13, HIGH);
+    }
+if(input =="GET")
 client.print(GetfromDB(host));
-} else {
+else
 client.print(SendtoDB(host));
 
   unsigned long timeout = millis();
@@ -130,7 +124,7 @@ client.print(SendtoDB(host));
 String json = ""; //De delarna vi vill ha ut av meddelandet sparar vi i stringen json
 boolean httpBody = false; //bool för att säa att vi har kommit ner till bodydelen
 // tittar om vi har anslutit till clienten
-while(client.available()) {
+while (client.available()) {
   String line = client.readStringUntil('\r'); //Läser varje rad tills det är slut på rader
   if (!httpBody && line.charAt(1) == '{') { //Om vi hittar { så vet vi att vi har nått bodyn
     httpBody = true; //boolen blir true för att vi ska veta för nästa rad att vi redan är i bodyn
@@ -139,43 +133,50 @@ while(client.available()) {
     json += line;
   }
 }
-
 //Skriver ut bodyns data
     Serial.println("Got data:");
     Serial.println(json);
-  if(input == "GET") //Om det är Get så kör vi metoden UpdateValues
+  if(input =="GET") //Om det är Get så kör vi metoden UpdateValues
     UpdateValues(json);
 
   Serial.println();
   Serial.println("closing connection");
-
 }
 
 void UpdateValues(String json){
+   Serial.println("1"+Lampname);
       //Vi skapar ett Jsonobjekt där vi klistrar in värdena från bodyn
-      Serial.print(LampName);
       StaticJsonBuffer<400> jsonBuffer;
     JsonObject& root = jsonBuffer.parseObject(json);
     //Vi skapar sedan lokala strings där vi lägger över värdena en i taget
-    String data1 = root["LampName"];
-         if(data1!="none")
+    String dataA = root["Name"];
+         if(dataA!="none")
          {
-      bool data2=root["Power"];
-      int data3=root["Hot"];
-      int data4=root["Cold"];    
-      LampName=data1;
-      Powervalue=data2;
-      Hotvalue=data3;
-      Coldvalue=data4;
-      LampExist=true;
-     Serial.print(Coldvalue);
-         }
+    bool dataB = root["Power"];
+    bool dataC = root["SensorSetting"];
+    int dataD = root["Timer1min"];
+    int dataE = root["Hot"];
+    int dataF = root["Cold"];
+    
+    //Därefter skriver vi över de lokala värdena till våra globala värden för lampan
+     Lampname = dataA; 
+     Powervalue =dataB;
+     Sensorsetting = dataC;
+     Timervalue = dataD;
+     Hotvalue = dataE;
+     Coldvalue = dataF;
+
+       LampExist=true;
+     Serial.print(Hotvalue);
+}
          else
          {
+
           String Mess =root["message"];
          Serial.print(Mess);
+
          }
-         Serial.print(LampName);
+         Serial.println("2"+Lampname);
   GottenValues = true;
 }
 
@@ -187,16 +188,15 @@ else
 }
 
 void loop() {
-ConnecttoDB("GET"); 
+ ConnecttoDB("GET"); 
   UpdatingLamp();
-  delay(10000);
- ConnecttoDB("POST");
-delay(10000);
+  delay(1000);
+  ConnecttoDB("POST");
+
 }
-
-
    //analog write, 0 -1023
 
-
+ 
   // digitalWrite(13, LOW);
 
+    
